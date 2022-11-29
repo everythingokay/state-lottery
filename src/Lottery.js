dfa05cbdf8b0ab45519aba0983c251ac9efac755
Alison Feldish <alison.feldish@gmail.com>
2022-11-28 21:37:54 -0500

    state logic & styling

--- src/Lottery.js
+++ src/Lottery.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
+import Ball from './Ball';
+import './Lottery.css';
 
 class Lottery extends Component {
     static defaultProps = {
-        title: 'lotto',
+        title: 'Lotto',
         maxBalls: 6,
         maxNum: 40
     };
     constructor(props) {
         super(props);
         this.state = { nums: Array.from({ length: this.props.maxBalls }) };
+        this.handleClick = this.handleClick.bind(this);
     };
     generate() {
-        //
+        this.setState(current => ({
+            nums: current.nums.map(n => Math.floor(Math.random() * this.props.maxNum) + 1)
+        }))
     };
     handleClick() {
         this.generate();
@@ -20,7 +25,9 @@ class Lottery extends Component {
         return (
             <section className='lottery'>
             <h1>{this.props.title}</h1>
-            <div></div>
+            <div>
+                {this.state.nums.map( n => <Ball num={n} />) }
+            </div>
             <button onClick={this.handleClick}>Generate</button>
             </section>
         )
